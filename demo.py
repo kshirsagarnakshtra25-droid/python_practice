213b70a1a549ab89e15b203e3140b0ed3f4c1e95
kshirsagarnakshtra25-droid <kshirsagarnakshtra@gmail.com>
2026-08-27 22:41:43 +0530

    added pthon folder again

---
+++ demo.py
@@ -0,0 +1,60 @@
+print("Hello")
+a=10
+print(type(a))
+
+
+name="nakshtra"
+print(type(name))
+
+
+a=1.1
+print(type(a))
+
+b= True
+print(type(b))
+
+name='''my name is
+ nakshtra'''
+print(name)
+
+
+a=111
+b=2
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+
+
+
+
+
+a=10
+b=20
+c=a+b
+print("addition {}".format(c))
+
+name=input('enter the name')
+print(name)
+
+num=int(input('enter num'))
+a=num//10
+b=num%10
+c=a+b
+print(c)
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
