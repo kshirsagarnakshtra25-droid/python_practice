0ed24431d5869923e2d00b4927eaef04bd4141af
kshirsagarnakshtra25-droid <kshirsagarnakshtra@gmail.com>
2026-08-27 22:49:46 +0530

    Fix day2 and day3 folders

---
+++ day2/day2.py
@@ -0,0 +1,53 @@
+#1.Personal Details Printer — Basic I/O
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# city = input("Enter your city: ")
+
+# print("My name is", name, "I am", age, "years old and I live in", city)
+
+
+# 2.Data Type Identifier
+# num = 10
+# price = 25.5
+# name = "Nakshtra"
+# is_student = True
+
+# print(type(num))
+# print(type(price))
+# print(type(name))
+# print(type(is_student))
+
+
+# 3. String to Number Conversion
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+# total = num1 + num2
+
+# print("Total =", total)
+
+
+
+# 4.Celsius to Fahrenheit
+# celsius = 43
+
+# fahrenheit = (celsius * 9 / 5) + 32
+
+# print("Temperature in Fahrenheit =", fahrenheit)
+
+
+# 5.Swap Two Variables
+
+# a = input("Enter value of a: ")
+# b = input("Enter value of b: ")
+
+# a, b = b, a
+
+# print("a =", a)
+# print("b =", b)
+
+
